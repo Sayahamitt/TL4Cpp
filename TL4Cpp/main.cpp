8ace9487c9f0d3ffb4f25880ed07e19d9dca13a8
Sayahamitt <sayahamitt@gmail.com>
2015-02-07 21:18:37 +0900

    Initial Commit

---
+++ TL4Cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TL4Cpp
+//
+//  Created by Sayahamitt on 2015/02/07.
+//  Copyright (c) 2015年 Sayahamitt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
