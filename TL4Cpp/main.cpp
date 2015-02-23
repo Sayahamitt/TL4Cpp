583f02c7f21b50d5c4fda861e6d4493fc0242387
Sayahamitt <sayahamitt@gmail.com>
2015-02-23 13:34:44 +0900

    HTTP のmultipart postのボディ部を生成する関数 makebody()を作成した。
    multipart form名前空間を切り、その中にmakebody(std::map<std::string,std::pair<std::string,std::string>>)を作成した。
    std::map<std::string,std::pair<std::string,std::string>>はmultipart_typeとしてtypedefしてあり、左から順にパラメータの名前, パラメータの中身(データ), データのHTTP Content-Type として指定する。
    バイナリデータの扱い
    	TwitterAPIを叩くシーンでの利用を想定しているため content-type が application/octet-stream と指定された場合のみContent-Typeの指定とファイル名を生成し付加している。

--- TL4Cpp/main.cpp
+++ TL4Cpp/main.cpp
@@ -12,6 +12,8 @@
 #include "clx/https.h"
 #include "clx/uri.h"
 
+#include "multipartform.h"
+
 #include "oauth.h"
 #include "getapi.h"
 #include "postapi.h"
@@ -23,13 +25,19 @@ const std::string my_access_sec ="cZPraKMj0s98BYCTxwtTxa8omaUorwdNTlbISLVtIDcoO"
 
 int main() {
     tl4cpp::oauth certifyer(c_key,c_sec,my_access_token,my_access_sec);
-    
+    std::pair<std::string,std::string> _pair("this is picture","application/octet-stream");
+    std::pair<std::string,std::string> _pair2("hello my status","");
+    std::map<std::string,std::pair<std::string,std::string>> test;
+    test["media"]=_pair;
+    test["status"]=_pair2;
+    std::cout<<tl4cpp::multipartform::makebody(test, tl4cpp::multipartform::strrand(32));
+    /*
     std::map<std::string,std::string> param;
     param["status"]="it is test";
     
     tl4cpp::postapi statusup(certifyer,"/statuses/update.json");
     statusup.request(param);
-    /*
+    
     std::map<std::string,std::string> param_a;
     param_a["count"]="1";
     
---
+++ TL4Cpp/multipartform.h
@@ -0,0 +1,58 @@
+//
+//  multipartform.h
+//  TL4Cpp
+//
+//  Created by Sayahamitt on 2015/02/23.
+//  Copyright (c) 2015年 Sayahamitt. All rights reserved.
+//
+
+#ifndef TL4Cpp_multipartform_h
+#define TL4Cpp_multipartform_h
+
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+namespace tl4cpp {
+    namespace multipartform{
+        /*multipart_type
+         std::map<パラメータの名前,std::pair<パラメータの中身,データのHTTP Content-Type>>
+         */
+        typedef std::map<std::string,std::pair<std::string,std::string>> multipart_type;
+        //ランダムな英小文字列生成関数
+        inline std::string strrand(int length){
+            char* _str = new char[length + 1];
+            time_t now_time;
+            time(&now_time);
+            srand((unsigned int)now_time);
+            for(int i=0;i<length + 1;i++){
+                _str[i] = rand() % 25 + 97;//UTF-8専用
+            }_str[length]=0;
+            
+            return std::string(_str);
+        }
+        
+        inline std::string makebody(const multipart_type& formdatas, std::string boundary){
+            //multipartform生成
+            std::string query;
+            
+            for(multipart_type::const_iterator itr = formdatas.begin(); itr!=formdatas.end(); itr++){
+                query += "--" + boundary +"\r\n";
+                query += "Content-Disposition: form-data; name=\""+itr->first+"\"";
+                if(itr->second.second == "application/octet-stream"){
+                    query +="; filename=\""+ strrand(10) +"\"\r\n";
+                    query += "Content-Type: " + itr->second.second + "\r\n";
+                }else{
+                    query += "\r\n";
+                }
+                query += "\r\n";
+                query += itr->second.first + "\r\n";
+            }
+            query += "--" + boundary + "--\r\n";
+            
+            return query;
+        }
+    }
+}
+#endif
